eaaca73450004edfdfa49b95fa435f7bc8f82163
Inchul <ijung@mapr.com>
2017-09-04 16:44:30 +0900

    :aot :all

--- project.clj
+++ project.clj
@@ -6,4 +6,5 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.drools/drools-core "6.5.0.Final"]
                  [org.drools/drools-compiler "6.5.0.Final"]]
+  :aot :all
   :main drools.rules.Drooltest)
